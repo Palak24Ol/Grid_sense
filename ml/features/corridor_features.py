6ae8d49ec1e243be6a75f5361216fb2eeab39cc6
Rishabh Kumar <kumarvrishabh700@gmail.com>
2026-06-23 11:44:15 +0530

    Enhance traffic feature engineering pipeline

--- ml/features/corridor_features.py
+++ ml/features/corridor_features.py
@@ -20,10 +20,11 @@ HIGH_PRIORITY_CORRIDORS: frozenset[str] = frozenset(
         "ORR East 2",
         "Magadi Road",
         "Old Madras Road",
-        "Bannerghata Road",
+        "Bannerghatta Road",
         "West of Chord Road",
         "CBD 2",
         "ORR West 1",
+        "ORR West 2",
     ]
 )
 
